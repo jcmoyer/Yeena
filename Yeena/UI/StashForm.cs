537c948c9fbddc818ed231427a002576112f04be
J.C. Moyer <jmoyer1992@gmail.com>
2013-03-25 04:10:32 -0400

    Use more specific exception for task cancellation

--- Yeena/UI/StashForm.cs
+++ Yeena/UI/StashForm.cs
@@ -73,7 +73,7 @@ namespace Yeena.UI {
             try {
                 if (_fetchTask != null)
                     await _fetchTask;
-            } catch (OperationCanceledException) {
+            } catch (TaskCanceledException) {
             }
 
             EnableUnsafeControls(false);
