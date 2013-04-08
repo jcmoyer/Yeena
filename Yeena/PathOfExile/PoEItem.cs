2cabe8d1ea9b8b5126878949075459ebf305d7b6
J.C. Moyer <jmoyer1992@gmail.com>
2013-04-08 02:24:14 -0400

    Disable warning 649 for files where the class fields are initialized by Json.net

--- Yeena/PathOfExile/PoEItem.cs
+++ Yeena/PathOfExile/PoEItem.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma warning disable 649
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Yeena/PathOfExile/PoEStashRequestError.cs
+++ Yeena/PathOfExile/PoEStashRequestError.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma warning disable 649
+
 using Newtonsoft.Json;
 
 namespace Yeena.PathOfExile {
